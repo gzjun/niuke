8baa9c97c3741a3d62a4a90e0dc2093d64048e77
gg <1263538583@qq.com>
2018-04-17 15:25:44 +0800

    add k min

---
+++ GetLeastNumbers_Solution.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class Solution {
+    // base on Partition
+
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] nums, int k)
+    {
+        if(nums.length<k)
+            return new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            pq.add(nums[i]);
+        }
+        for(int i=0;i<k;i++)
+            res.add(pq.poll());
+        return res;
+    }
+}
