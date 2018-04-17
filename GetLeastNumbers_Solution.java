4f2ea79c73d4bed14552aaa6a364e88b542e45e8
gg <1263538583@qq.com>
2018-04-17 15:28:11 +0800

    add describe to the k min

--- GetLeastNumbers_Solution.java
+++ GetLeastNumbers_Solution.java
@@ -2,8 +2,7 @@ import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 public class Solution {
-    // base on Partition
-
+    // base on Partition has run out of time, and the PriorityQueue is used to solve this problem
     public ArrayList<Integer> GetLeastNumbers_Solution(int [] nums, int k)
     {
         if(nums.length<k)
